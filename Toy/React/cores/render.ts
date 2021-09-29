945fd1ba14f9eb3020b9b457a00e072157c1f073
jinzenan3 <jinzenan3@jd.com>
2021-09-29 14:45:43 +0800

    运行时，无法使用自定义的jsx：pragma and pragmaFrag cannot be set when runtime is automatic

--- Toy/React/cores/render.ts
+++ Toy/React/cores/render.ts
@@ -1,12 +1,23 @@
 import {TEXT_ELEMENT_TYPE,isProperty} from '../../Common/constant'
-const render=(virtualDom,container)=>{
-    const dom=virtualDom.type===TEXT_ELEMENT_TYPE?document.createTextNode(""):document.createElement(element.type);
-    Object.keys(virtualDom.props)
-    .filter(isProperty)
-    .forEach((propertyName)=>{
-        dom[propertyName]=element.props[propertyName]
-    })
-    virtualDom.props.children.forEach((children)=>render(children,dom))
+function render(element, container) {
+    // 1. 创建不同类型的DOM节点
+    const dom =
+      element.type == "TEXT_ELEMENT"
+        ? document.createTextNode("")
+        : document.createElement(element.type);
+  
+    // 2.为 DOM 节点添加属性props (没有children属性)
+    const isProperty = (key) => key !== "children";
+    Object.keys(element.props)
+      .filter(isProperty)
+      .forEach((name) => {
+        dom[name] = element.props[name];
+      });
+  
+    // 3. 遍历children，递归调用 render
+    element.props.children.forEach((child) => render(child, dom));
+  
+    // 4. 将 DOM 节点添加至 root 根节点
     container.appendChild(dom);
-}
+  }
 export default render
