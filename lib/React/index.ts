40d5425c737bd151f46d9671aa763a470c9da438
waltiu <waltiu@163.com>
2022-04-26 17:28:04 +0800

    chore: 修改入口

--- lib/React/index.ts
+++ lib/React/index.ts
@@ -1,10 +1,14 @@
 import createElement from './cores/createElement'
 import render from './cores/render'
 import useState from './cores/hooks/usestate'
+const log=()=>{
+    console.log('jreact')
+}
 const JReact ={
     createElement,
     render,
-    useState
+    useState,
+    log
     
 }
 export default JReact
