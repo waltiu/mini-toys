e4942672a60a301827c2b0d70354719cfa33b931
waltiu <waltiu@163.com>
2022-04-29 14:06:25 +0800

    chore: 重学react

--- lib/React/index.ts
+++ lib/React/index.ts
@@ -1,14 +1,8 @@
 import createElement from './cores/createElement'
 import render from './cores/render'
-import useState from './cores/hooks/usestate'
-const log=()=>{
-    console.log('jreact')
-}
+
 const JReact ={
     createElement,
     render,
-    useState,
-    log
-    
 }
 export default JReact
---
+++ src/Page/Home/index.tsx
@@ -0,0 +1,12 @@
+/** @jsxRuntime classic */  
+/** @jsx React.createElement */
+import {React} from 'mini-toys'
+
+const Home=()=>{
+    return (
+        <div>
+            11
+        </div>
+    )
+}
+export default Home
